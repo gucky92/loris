fddafeae5c3f1029ebf3be95770a0551b8c9644d
gucky92 <gucky@gucky.eu>
2020-05-07 13:08:55 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 from setuptools import setup
 import os
 import sys
+import subprocess
 
 min_py_version = (3, 7)
 
@@ -18,14 +19,13 @@ long_description = 'Data management and analysis application for DataJoint'
 with open(os.path.join(path, 'requirements.txt')) as f:
     requirements = f.read().split()
 
-
 setup(name='loris',
       version='0.1',
       description='Loris',
       long_description=long_description,
       author='Matthias Christenson',
       author_email='gucky@gucky.eu',
-      install_requires=requirements,
-      keywords='database application', 
+      install_requires=requirements[:-1]+['datajoint @ ' + requirements[-1]],
+      keywords='database application',
       packages=['loris']
 )
