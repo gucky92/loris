24ea5bf1de2c1bd3dfca5b5e02a65eaf1a8da550
gucky92 <gucky@gucky.eu>
2020-06-10 10:53:58 +0200

    fix to setup

--- setup.py
+++ setup.py
@@ -27,5 +27,5 @@ setup(name='loris',
       author_email='gucky@gucky.eu',
       install_requires=requirements[:-1]+['datajoint @ ' + requirements[-1]],
       keywords='database application',
-      packages=find_namespace_packages(include=["loris.*"])
+      packages=find_namespace_packages(include=["loris", "loris.*"])
 )
