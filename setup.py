8dd5478ce938d0f4b8dafeaf2b5cda7f7abf4b4a
gucky92 <gucky@gucky.eu>
2021-01-15 00:25:14 +0100

    make pip installable

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+from subprocess import getoutput
 import os
 import sys
 
@@ -18,6 +20,15 @@ long_description = 'Data management and analysis application for DataJoint'
 with open(os.path.join(path, 'requirements.txt')) as f:
     requirements = f.read().split()
 
+
+class PostInstall(install):
+    pkgs = f" {requirements[-1]}"
+
+    def run(self):
+        install.run(self)
+        print(getoutput('pip install' + self.pkgs))
+
+
 setup(
     name='loris',
     version='0.0.7',
@@ -25,8 +36,10 @@ setup(
     long_description=long_description,
     author='Matthias Christenson',
     author_email='gucky@gucky.eu',
-    install_requires=requirements[:-1] + ['datajoint @ ' + requirements[-1]],
+    install_requires=requirements[:-1],
     keywords='database application',
     packages=find_packages(exclude=["docs", "images", "tests"]),
     include_package_data=True,
+    scripts=['bin/lorisapp'],
+    cmdclass={'install': PostInstall}
 )
