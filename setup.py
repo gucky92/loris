42d5232ad1acfc228c1bab0c71d17220ab1d1b8e
gucky92 <gucky@gucky.eu>
2021-01-15 09:19:37 +0100

    changes to requirements file

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-from setuptools.command.install import install
-from subprocess import getoutput
 import os
 import sys
 
@@ -21,14 +19,6 @@ with open(os.path.join(path, 'requirements.txt')) as f:
     requirements = f.read().split()
 
 
-class PostInstall(install):
-    pkgs = f" {requirements[-1]}"
-
-    def run(self):
-        install.run(self)
-        print(getoutput('pip install' + self.pkgs))
-
-
 setup(
     name='lorisapp',
     version='0.0.7',
@@ -36,12 +26,9 @@ setup(
     long_description=long_description,
     author='Matthias Christenson',
     author_email='gucky@gucky.eu',
-    install_requires=requirements[:-1],
+    install_requires=requirements,
     keywords='database application',
     packages=find_packages(exclude=["docs", "images", "tests"]),
     include_package_data=True,
     scripts=['bin/lorisapp'],
-    cmdclass={
-        'install': PostInstall
-    }
 )
