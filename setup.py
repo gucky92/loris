65bb558c0625536fa9e863ab62413919ab8957fd
gucky92 <gucky@gucky.eu>
2021-02-05 17:40:13 +0100

    fix serialization

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open(os.path.join(path, 'requirements.txt')) as f:
 
 setup(
     name='lorisapp',
-    version='0.0.9',
+    version='0.0.10',
     description='Loris',
     long_description=long_description,
     author='Matthias Christenson',
