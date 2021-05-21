22dce5bac2ab4c15077d8e8ea667953a5f514705
gucky92 <gucky@gucky.eu>
2021-05-21 16:51:40 -0400

    bump version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open(os.path.join(path, 'requirements.txt')) as f:
 
 setup(
     name='lorisapp',
-    version='0.1.3',
+    version='0.1.4dev1',
     description='Loris',
     long_description=long_description,
     author='Matthias Christenson',
