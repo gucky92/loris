0530335bfbc7fd15c8a17bc3b2a4d50fa70d2b7a
gucky92 <gucky@gucky.eu>
2022-07-27 13:37:33 -0400

    bump version

--- loris/__init__.py
+++ loris/__init__.py
@@ -3,7 +3,7 @@
 
 __author__ = """gucky92"""
 __email__ = 'gucky@gucky.eu'
-__version__ = '0.1.7'
+__version__ = '0.1.8'
 
 import os
 
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open(os.path.join(path, 'requirements.txt')) as f:
 
 setup(
     name='lorisapp',
-    version='0.1.7',
+    version='0.1.8',
     description='Loris',
     long_description=long_description,
     author='Matthias Christenson',
